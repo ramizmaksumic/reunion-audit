1213f3d38d0c90507ec5e7c5fd73530a2510d171
Ramiz <ramiz.maksumic@gmail.com>
2026-09-24 13:26:33 +0200

    Update application favicon

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,8 +5,7 @@
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
+<link rel="icon" type="image/x-icon" href="/favicon.ico?v=2">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
 @fonts
